0376543c73137ce86a9a67c8b7f5abb97fffb96c
Nikolay Akopov <asoman97@gmail.com>
2016-02-25 17:14:14 +0300

    Более интуитивный чекбок, йеееей!

--- tmp/login.tpl.php
+++ tmp/login.tpl.php
@@ -5,7 +5,7 @@
 		<p id = "password-label" class = "login-label">Пароль должен содержать минимум 6 символов</p>
         <input type="password" placeholder="Ваш пароль" id ="password" class="textfield" name="password" size="35">
         <label for="remember">
-            <input id="check" name="remember" value="1" type="checkbox"> <label for="check">Запомнить меня</label>
+            <input id="check" name="remember" value="1" type="checkbox"> <label class= "checkbox" for="check"></label> <span>Запомнить меня</span>
         </label>
 		<button type="submit" class="login-submit" name="login">Войти в кабинет</button>
         <a href="recover"class="login-button recover-button">Забыл пароль</a> <a href="signup" class="login-button signup-button">Регистрация</a>
